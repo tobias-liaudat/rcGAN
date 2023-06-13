8c898f037e51eb1a7bd6efc135be427647e49177
Tobias Liaudat <tobiasliaudat@gmail.com>
2023-06-13 13:31:35 +0100

    Update script to separate into train val and test dirs

--- mass_map_utils/preprocessing/kappa_selection.py
+++ mass_map_utils/preprocessing/kappa_selection.py
@@ -18,23 +18,51 @@ if not os.path.exists(dst_path):
    os.makedirs(dst_path)
    print("The new directory has been made!")
 
+dst_train_path = dst_path + 'kappa_train/'
+dst_test_path = dst_path + 'kappa_test/'
+dst_val_path = dst_path + 'kappa_val/'
+
+if not os.path.exists(dst_train_path):
+    os.makedirs(dst_train_path)
+if not os.path.exists(dst_test_path):
+    os.makedirs(dst_test_path)
+if not os.path.exists(dst_val_path):
+    os.makedirs(dst_val_path)
+
 
 img_number = 1
+total_nb_files = len(all_files)
 ng = 1024
+
+# Set seed
+np.random.seed(0)
+# Shuffle files
+np.random.shuffle(all_files)
+
 
 for fname in all_files:
     print('Processing file n', img_number)
 
     with open(fname, 'rb') as f:
 
-
         dummy = np.fromfile(f, dtype="int32", count=1)
         kappa = np.fromfile(f, dtype="float", count=ng*ng)
         dummy = np.fromfile(f, dtype="int32", count=1)
 
         kappa = kappa.reshape((ng,ng))
-    
-        save_path = '{:s}{:s}{:05d}{:s}'.format(dst_path, "kappa_run_", img_number, ".npy")
+
+        # Using 85% of data for training
+        if (img_number/total_nb_files) <= 0.85:
+            dst_dir = dst_train_path
+        # Using 10% of data for testing
+        elif (img_number/total_nb_files) > 0.85 and (img_number/total_nb_files) <= 0.95:
+            dst_dir = dst_test_path
+        # Using 5% of data for validation
+        else:
+            dst_dir = dst_val_path
+
+        save_path = '{:s}{:s}{:05d}{:s}'.format(dst_dir, "kappa_run_", img_number, ".npy")
+        
         np.save(save_path, kappa, allow_pickle=True)
         
         img_number +=1
