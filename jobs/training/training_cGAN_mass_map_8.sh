1f9d0a3fb6fbca975adb33d4d9d6ea252621fdeb
Jessica Whitney <jjwhit@hypatia-login.local>
2023-06-29 12:13:54 +0100

    Updates bash files

---
+++ jobs/training/training_cGAN_mass_map_8.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#SBATCH --job-name=tr_cGAN_8
+#SBATCH -p GPU
+# requesting one node
+# SBATCH -N1
+# requesting 12 cpus
+# SBATCH -n12
+#SBATCH --ntasks=1                   # nombre total de tache MPI (= nombre total de GPU)
+#SBATCH --ntasks-per-node=1          # nombre de tache MPI par noeud (= nombre de GPU par noeud)
+#SBATCH --cpus-per-task=16           # nombre de coeurs CPU par tache (un quart du noeud ici)
+#SBATCH –nodelist=compute-gpu-0-4
+#SBATCH --gres=gpu:a100:4            # requesting GPUs
+##SBATCH --mem-per-gpu=80GB           # memory per GPU
+#SBATCH --mail-use=jessica.whitney.22@ucl.ac.uk
+#SBATCH --mail-type=ALL
+#SBATCH --output=tr_cGAN_8_%j.out
+#SBATCH --error=tr_cGAN_8_%j.err
+
+
+
+# Load conda and activate environment
+source /share/apps/anaconda/3-2022.05/etc/profile.d/conda.sh
+conda activate cGAN
+
+#export WANDB_DIR=/share/gpu0/tl3/wandb/logs
+#export WANDB_CACHE_DIR=/share/gpu0/tl3/wandb/.cache/wandb
+#export WANDB_CONFIG_DIR=/share/gpu0/tl3/wandb/.config/wandb
+
+# Echo commands
+set -x
+
+echo $CUDA_VISIBLE_DEVICES
+echo $WANDB_DIR
+echo $WANDB_CACHE_DIR
+echo $WANDB_CONFIG_DIR
+
+cd /share/gpu0/jjwhit/rcGAN
+
+srun python -u train.py --mass_mapping_8 --exp-name mmgan_training_8 --num-gpus 4
