06412e5dc3d1b08840b0e6a0db592d2a288128a2
Jessica Whitney <jjwhit@hypatia-login.local>
2023-07-03 15:13:00 +0100

    Configures different options for kappa processing

--- mass_map_utils/preprocessing/kappa_selection.py
+++ mass_map_utils/preprocessing/kappa_selection.py
@@ -46,16 +46,16 @@ np.random.shuffle(all_files)
 
 #Includes the transformation from .dat to .npy
 
-for fname in all_files:
+""" for fname in all_files:
    
     print('Processing file n', img_number)
-
     with open(fname, 'rb') as f:
         dummy = np.fromfile(f, dtype="int32", count=1)
         kappa = np.fromfile(f, dtype="float", count=ng*ng)
         dummy = np.fromfile(f, dtype="int32", count=1)
 
         kappa = kappa.reshape((ng,ng))
+        kappa = f
         kappa[kappa>0.7]=0.7
 
         center_size = 384  #Size of MRI images
@@ -79,23 +79,32 @@ for fname in all_files:
         
         np.save(save_path, kappa_cropped, allow_pickle=True)
         
-        img_number +=1
+        img_number +=1 """
 
-#for fname in all_files:
-#    print('Processing file n', img_number)
-#    data = np.load(fname, allow_pickle=True)
+#Takes kappa.npy files and crops them
+for fname in all_files:
+    print('Processing file n', img_number)
+    kappa = np.load(fname, allow_pickle=True)
+    kappa[kappa>0.7]=0.7
 
-#    if (img_number/total_nb_files) <= 0.85:
-#        dst_dir = dst_train_path
-#    # Using 10% of data for testing
-#    elif (img_number/total_nb_files) > 0.85 and (img_number/total_nb_files) <= 0.95:
-#        dst_dir = dst_test_path
-#    # Using 5% of data for validation
-#    else:
-#        dst_dir = dst_val_path
+    center_size = 384  #Size of MRI images
+    center_start = (1024 - center_size) // 2
+    center_end =  center_start + center_size
 
-#    save_path = '{:s}{:s}{:05d}{:s}'.format(dst_dir, "kappa_run_", img_number, ".npy")
-#    
-#    np.save(save_path, data, allow_pickle=True)
+    kappa_cropped = kappa[center_start:center_end,  center_start:center_end]
+            # Using 85% of data for training
+    if (img_number/total_nb_files) <= 0.85:
+        dst_dir = dst_train_path
+        # Using 10% of data for testing
+    elif (img_number/total_nb_files) > 0.85 and (img_number/total_nb_files) <= 0.95:
+        dst_dir = dst_test_path
+        # Using 5% of data for validation
+    else:
+        dst_dir = dst_val_path
+
+    save_path = '{:s}{:s}{:05d}{:s}'.format(dst_dir, "kappa_run_", img_number, ".npy")
     
-#    img_number +=1
+    np.save(save_path, kappa_cropped, allow_pickle=True)
+    
+    img_number +=1
+
