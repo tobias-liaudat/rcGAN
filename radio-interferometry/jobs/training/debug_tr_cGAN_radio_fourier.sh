1c8a9e80523ca3b7261abbba2d4804e7cbe9db2c
Tobias Liaudat <tobiasliaudat@gmail.com>
2023-08-31 15:08:20 +0100

    add jobs for fourier and radio options

--- radio-interferometry/jobs/training/debug_tr_cGAN_radio_fourier.sh
+++ radio-interferometry/jobs/training/debug_tr_cGAN_radio_fourier.sh
@@ -11,12 +11,12 @@
 #SBATCH --ntasks-per-node=1          # nombre de tache MPI par noeud (= nombre de GPU par noeud)
 #SBATCH --cpus-per-task=16           # nombre de coeurs CPU par tache (un quart du noeud ici)
 ##SBATCH --gres=gpu:a100:4            # requesting GPUs
-#SBATCH --gres=gpu:4            # requesting GPUs
+#SBATCH --gres=gpu:1            # requesting GPUs
 ##SBATCH --mem-per-gpu=80GB           # memory per GPU
 #SBATCH --mail-use=t.liaudat@ucl.ac.uk
 #SBATCH --mail-type=ALL
-#SBATCH --output=tr_radio_%j.out
-#SBATCH --error=tr_radio_%j.err
+#SBATCH --output=tr_radio_fourier_%j.out
+#SBATCH --error=tr_radio_fourier_%j.err
 
 
 
@@ -38,4 +38,4 @@ echo $WANDB_CONFIG_DIR
 
 cd /home/tl3/repos/project-rcGAN/matthijs_version/rcGAN
 
-srun python -u train.py --radio --exp-name radio_train_debug --num-gpus 4
+srun python -u train.py --radio_fourier --exp-name radio_train_debug --num-gpus 1
