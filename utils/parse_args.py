0ff1894b6f4ab2d5a5c962bc53025026f7cfa9d6
Tobias Liaudat <tobiasliaudat@gmail.com>
2023-11-07 11:25:48 +0000

    fix argument bug

--- utils/parse_args.py
+++ utils/parse_args.py
@@ -10,7 +10,7 @@ def create_arg_parser():
     parser.add_argument('--resume', action='store_true',
                         help='Whether or not to resume training.')
     parser.add_argument('--resume-epoch', default=0, type=int, help='Epoch to resume training from')
-    parser.add_argument('--config', type=str, default='./configs/mri.yml', action='store_true', help='Path to the config file') 
+    parser.add_argument('--config', type=str, default='./configs/mri.yml', help='Path to the config file') 
     parser.add_argument('--exp-name', type=str, default="default_exp_name", help='Name for the run.')
     parser.add_argument('--num-gpus', default=1, type=int, help='The number of GPUs to use during training.')
     parser.add_argument('--num-figs', default=1, type=int, help='The number of figures to generate while plotting.')
