2f2598b9fcc70663b2a26d41f9582d77c1319425
Jess Whitney <jess.j.whitney@gmail.com>
2024-04-17 00:17:34 +0100

    No longer need mean,std.cuda as they are floats

--- scripts/mass_map/gen_cosmos_samps.py
+++ scripts/mass_map/gen_cosmos_samps.py
@@ -9,16 +9,11 @@ import sys
 sys.path.append('/home/jjwhit/rcGAN/')
 
 from data.lightning.MassMappingDataModule import MMDataModule
-from data.lightning.MassMappingDataModule import MMDataTransform #import compute_fourier_kernel, realistic_noise_maker
 from utils.parse_args import create_arg_parser
 from pytorch_lightning import seed_everything
 from models.lightning.mmGAN import mmGAN
 from utils.mri import transforms
 import time
-
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-from skimage.measure import find_contours
 
 def load_object(dct):
     return types.SimpleNamespace(**dct)
@@ -53,31 +48,15 @@ if __name__ == "__main__":
 #Step 2: Load cosmos shear map
     cosmos_shear = np.load('/home/jjwhit/rcGAN/mass_map_utils/cosmos/cosmos_shear_cropped.npy')
     cosmos_shear_tensor = transforms.to_tensor(cosmos_shear)
-    cosmos_shear_tensor = cosmos_shear_tensor.permute(2, 0, 1)
-    cosmos_shear_tensor = cosmos_shear_tensor[None,:, :, :].cuda()
+    cosmos_shear_tensor = cosmos_shear_tensor.permute(2, 0, 1).cuda()
 
 #Step 3: Feed through GAN and generate 32 samples.
 
-    #TODO: Load the mean and std.
-    normalized_gamma, mean, std = transforms.normalize_instance(cosmos_shear_tensor)
-    normalized_gamma = normalized_gamma.cuda()
-    mean = mean.cuda()
-    std = std.cuda()
+    normalized_gamma, mean, std = transforms.normalise_instance(cosmos_shear_tensor)
+    normalized_gamma = normalized_gamma[None,:,:,:].cuda()
 
     gens_mmGAN = torch.zeros(size=(cfg.num_z_test, cfg.im_size, cfg.im_size, 2)).cuda()
-    # for z in range(cfg.num_z_test):
-    #     gens_mmGAN[z, :, :, :] = mmGAN_model.reformat(mmGAN_model.forward(normalized_gamma).cuda())
-    #normalized gamma or cosmos_shear_tensor?
-    batch_size = 32  # or any other suitable value
+    for z in range(cfg.num_z_test):
+        gens_mmGAN[z, :, :, :] = mmGAN_model.reformat(mmGAN_model.forward(normalized_gamma).cuda())
 
-    start_time = time.time()
-    for z in range(0, cfg.num_z_test, batch_size):
-        gens_mmGAN[z:z+batch_size, :, :, :] = mmGAN_model.reformat(mmGAN_model.forward(normalized_gamma).cuda())
-    total_time = time.time() - start_time
-    print(f'total time is: {total_time}')
-
-    torch.save('/home/jjwhit/rcGAN/mass_map_utils/cosmos/cosmos_samps_400.npy', gens_mmGAN)
-
-    # avg_mmGAN = torch.mean(gens_mmGAN, dim=1)
-
-    # zfr = mmGAN_model.reformat(cosmos_shear_tensor)
+    torch.save(gens_mmGAN,'/home/jjwhit/rcGAN/mass_map_utils/cosmos/cosmos_samps_manual_2')
