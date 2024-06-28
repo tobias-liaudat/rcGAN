4c312f11d23788adcc94eabe5c5ae16925059d64
Jessica Whitney <jessica.whitney.22@ucl.ac.uk>
2024-06-28 14:10:59 +0100

    Updates validation to include other metrics

--- scripts/mass_map/validate.py
+++ scripts/mass_map/validate.py
@@ -6,8 +6,10 @@ import json
 import numpy as np
 
 import sys
-sys.path.append('/home/jjwhit/rcGAN/')
 
+sys.path.append("/home/jjwhit/rcGAN/")
+
+from mass_map_utils.scripts.ks_utils import pearsoncoeff, psnr, snr, rmse
 from data.lightning.MassMappingDataModule import MMDataModule
 from utils.parse_args import create_arg_parser
 from models.lightning.mmGAN import mmGAN
@@ -15,17 +17,19 @@ from pytorch_lightning import seed_everything
 from utils.embeddings import VGG16Embedding
 from evaluation_scripts.mass_map_cfid.cfid_metric import CFIDMetric
 
+
 def load_object(dct):
     return types.SimpleNamespace(**dct)
 
+
 if __name__ == "__main__":
-    torch.set_float32_matmul_precision('medium')
+    torch.set_float32_matmul_precision("medium")
     args = create_arg_parser().parse_args()
     seed_everything(1, workers=True)
 
     config_path = args.config
 
-    with open(config_path, 'r') as f:
+    with open(config_path, "r") as f:
         cfg = yaml.load(f, Loader=yaml.FullLoader)
         cfg = json.loads(json.dumps(cfg), object_hook=load_object)
 
@@ -35,16 +39,25 @@ if __name__ == "__main__":
     best_epoch = -1
     inception_embedding = VGG16Embedding()
     best_cfid = 10000000
-    start_epoch = 80 #Will start saving models after 70 epochs
+    best_pearson = -1
+    start_epoch = 80  # Will start saving models after 80 epochs
     end_epoch = 100
-
+    mask = np.load(
+        self.args.cosmo_dir_path + "cosmos_mask.npy", allow_pickle=True
+    ).astype(bool)
 
     with torch.no_grad():
-        
+
         for epoch in range(start_epoch, end_epoch):
             print(f"VALIDATING EPOCH: {epoch}")
+
+            # Loads the model one epoch at a time
             try:
-                model = mmGAN.load_from_checkpoint(checkpoint_path=cfg.checkpoint_dir + args.exp_name + f'/checkpoint-epoch={epoch}.ckpt')
+                model = mmGAN.load_from_checkpoint(
+                    checkpoint_path=cfg.checkpoint_dir
+                    + args.exp_name
+                    + f"/checkpoint-epoch={epoch}.ckpt"
+                )
             except Exception as e:
                 print(e)
                 continue
@@ -64,18 +77,44 @@ if __name__ == "__main__":
                 cuda=True,
                 args=cfg,
                 ref_loader=False,
-                num_samps=1
+                num_samps=1,
             )
+
+            reconstruction, label, truth = cfid_metric._get_generated_distribution()
 
             cfids = cfid_metric.get_cfid_torch_pinv()
 
             cfid_val = np.mean(cfids)
 
             if cfid_val < best_cfid:
-                best_epoch = epoch
+                best_epoch_cfid = epoch
                 best_cfid = cfid_val
 
-    print(f"BEST EPOCH: {best_epoch}")
+            pearson_val = pearsoncoeff(truth, reconstruction, mask)
+            if pearson_val > best_pearson:
+                best_epoch_pearson = epoch
+                best_pearson = pearson_val
+
+            psnr_val = psnr(truth, reconstruction, mask)
+            if psnr_val > best_psnr:
+                best_epoch_psnr = epoch
+                best_psnr = psnr_val
+
+            snr_val = snr(truth, reconstruction, mask)
+            if snr_val > best_snr:
+                best_epoch_snr = epoch
+                best_snr = snr_val
+
+            rmse_val = rmse(truth, reconstruction, mask)
+            if rmse_val < best_rmse:
+                best_epoch_rmse = epoch
+                best_rmse = rmse_val
+
+    print(f"BEST EPOCH FOR CFID: {best_epoch_cfid}")
+    print(f"BEST EPOCH FOR PSNR: {best_epoch_psnr}")
+    print(f"BEST EPOCH FOR SNR: {best_epoch_snr}")
+    print(f"BEST EPOCH FOR RMSE: {best_epoch_rmse}")
+    print(f"BEST EPOCH FOR R: {best_epoch_pearson}")
 
     # for epoch in range(end_epoch):
     #     try:
@@ -85,6 +124,6 @@ if __name__ == "__main__":
     #         pass
 
     os.rename(
-        cfg.checkpoint_dir + args.exp_name + f'/checkpoint-epoch={best_epoch}.ckpt',
-        cfg.checkpoint_dir + args.exp_name + f'/checkpoint_best.ckpt'
+        cfg.checkpoint_dir + args.exp_name + f"/checkpoint-epoch={best_epoch}.ckpt",
+        cfg.checkpoint_dir + args.exp_name + f"/checkpoint_best.ckpt",
     )
