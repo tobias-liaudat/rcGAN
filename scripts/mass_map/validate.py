667f6bfb00ccccf7620f8a3243e2d0f824eb0528
Jessica Whitney <jessica.whitney.22@ucl.ac.uk>
2024-06-28 14:16:08 +0100

    Adds table to track metrics for each epoch

--- scripts/mass_map/validate.py
+++ scripts/mass_map/validate.py
@@ -40,11 +40,20 @@ if __name__ == "__main__":
     inception_embedding = VGG16Embedding()
     best_cfid = 10000000
     best_pearson = -1
+    best_psnr = -1
+    best_snr = -1
+    best_rmse = 10000000
     start_epoch = 80  # Will start saving models after 80 epochs
     end_epoch = 100
     mask = np.load(
         self.args.cosmo_dir_path + "cosmos_mask.npy", allow_pickle=True
     ).astype(bool)
+
+    cfid_vals = []
+    psnr_vals = []
+    snr_vals = []
+    rmse_vals = []
+    r_vals = []
 
     with torch.no_grad():
 
@@ -91,21 +100,25 @@ if __name__ == "__main__":
                 best_cfid = cfid_val
 
             pearson_val = pearsoncoeff(truth, reconstruction, mask)
+            pearson_vals.append((epoch, pearson_val))
             if pearson_val > best_pearson:
                 best_epoch_pearson = epoch
                 best_pearson = pearson_val
 
             psnr_val = psnr(truth, reconstruction, mask)
+            psnr_vals.append((epoch, psnr_val))
             if psnr_val > best_psnr:
                 best_epoch_psnr = epoch
                 best_psnr = psnr_val
 
             snr_val = snr(truth, reconstruction, mask)
+            snr_vals.append((epoch, snr_val))
             if snr_val > best_snr:
                 best_epoch_snr = epoch
                 best_snr = snr_val
 
             rmse_val = rmse(truth, reconstruction, mask)
+            rmse_vals.append((epoch, rmse_val))
             if rmse_val < best_rmse:
                 best_epoch_rmse = epoch
                 best_rmse = rmse_val
@@ -115,6 +128,17 @@ if __name__ == "__main__":
     print(f"BEST EPOCH FOR SNR: {best_epoch_snr}")
     print(f"BEST EPOCH FOR RMSE: {best_epoch_rmse}")
     print(f"BEST EPOCH FOR R: {best_epoch_pearson}")
+
+    print("Epoch | CFID | PSNR | SNR | RMSE | R")
+    for epoch, cfid, psnr, snr, rmse, r in zip(
+        range(start_epoch, end_epoch),
+        cfid_vals,
+        psnr_vals,
+        snr_vals,
+        rmse_vals,
+        r_vals,
+    ):
+        print(f"{epoch} | {cfid} | {psnr} | {snr} | {rmse} | {r}")
 
     # for epoch in range(end_epoch):
     #     try:
